9f51cef4aeca9409ef094131a3db2460b6a80bb5
TheStarBoys <shengzang19980@163.com>
2023-04-03 21:30:11 +0800

    Add deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,14 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const baseURI = "https://test.url.com";
+  const Role = await ethers.getContractFactory("Role");
+  const role = await Role.deploy(baseURI);
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
+  await role.deployed();
 
   console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    `Role deployed to ${role.address}`
   );
 }
 
