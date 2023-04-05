8d752a762be7fb3b82f2832e1c10f611e4b8621e
TheStarBoys <shengzang19980@163.com>
2023-04-05 22:56:13 +0800

    Initialize dapp

---
+++ dapp/pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>NFT Battle Game Demo</title>
+        <meta name="description" content="An NFT battle game demo written by TheStarBoys" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <h1>Hello World!!!</h1>
+      </main>
+    </>
+  )
+}
